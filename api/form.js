97cc2d9c89342709046a3c77311861f14b763eb0
Rocio Noelia <roccynoeli@gmail.com>
2026-04-11 01:08:10 -0300

    Agrego type module al package.json

--- api/form.js
+++ api/form.js
@@ -1,7 +1,7 @@
-import formidable from 'formidable';
-import fs from 'fs';
+const formidable = require('formidable');
+const fs = require('fs');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -82,4 +82,4 @@ export default async function handler(req, res) {
     console.error('ERROR EN /api/form:', error);
     return res.status(500).json({ error: error.message || 'Error en el servidor' });
   }
-}
+};
